6a7331b6146af98bd40fe3ad640ed77c31afa953
Haidar Ahmad Faiq <haidarahmadfaiq@gmail.com>
2023-11-20 01:11:21 +0700

    add file index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+     MIT License
+ * Copyright (c) 2023 Haidar Ahmad Faiq * 
+ ---------- GeRandChars ---------- 
+*/
+
+const randStr = require('./lib/randStr.js');
+const randNum = require('./lib/randNum.js');
+const randPasswd = require('./lib/randPasswd.js');
+const randStrNum = require('./lib/randStrNum.js');
+
+module.exports = {
+  randStr,
+  randNum,
+  randPasswd,
+  randStrNum,
+}
